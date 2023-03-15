63ab9289aa613ba0e8e1cd383f4d5c52bdd7a932
VKTHU <vuthikieuthu1999@gmail.com>
2023-03-15 21:47:04 +0700

    update

--- selenium/webdriver/Topic_13_Action_Part1.java
+++ selenium/webdriver/Topic_13_Action_Part1.java
@@ -1,10 +1,13 @@
 package webdriver;
 
 import java.awt.Desktop.Action;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -48,7 +51,7 @@ public class Topic_13_Action_Part1 {
 		Assert.assertEquals(driver.findElement(By.cssSelector("span.breadcrumbs-crumb")).getText(), "Kids Home Bath");
 	
 	}
-	@Test
+	//@Test
 		public void TC_03_Fahasa() {
 		driver.get("https://www.fahasa.com/");
 		sleepInSecond(10);
@@ -60,6 +63,44 @@ public class Topic_13_Action_Part1 {
 		Assert.assertTrue(driver.findElement(By.xpath("//ol[@class='breadcrumb']//strong[text()='Kỹ năng sống']")).isDisplayed());
 		}
     
+	@Test
+	public void TC_04_Click_and_hold() {
+		driver.get("https://automationfc.github.io/jquery-selectable/");
+		List<WebElement> listNumber = driver.findElements(By.xpath("//ol[@id='selectable']/li"));
+		//Click vào số đầu tiên
+		action.clickAndHold(listNumber.get(0))
+		//Di tới số 8
+		.moveToElement(listNumber.get(7))
+		//Nhả chuột ra
+	    .release()
+	    //Thực thi câu lệnh
+	    .perform();
+		sleepInSecond(2);
+		//Verify
+		List<WebElement> lisSelecttNumber = driver.findElements(By.xpath("//ol[@id='selectable']/li[@class='ui-state-default ui-selectee ui-selected']"));
+		Assert.assertEquals(lisSelecttNumber.size(), 8);
+	}
+	@Test
+	public void TC_05_Click_and_select_random_items() {
+		driver.get("https://automationfc.github.io/jquery-selectable/");
+		List<WebElement> listNumber = driver.findElements(By.xpath("//ol[@id='selectable']/li"));
+		//Nhấn ctrl xuống 
+		action.keyDown(Keys.CONTROL).perform();
+		//Chọn số random
+		action.click(listNumber.get(0))
+		.click(listNumber.get(5))
+		.click(listNumber.get(7))
+		.click(listNumber.get(10));
+		//Nhả phím ctrl
+		action.keyUp(Keys.CONTROL).perform();
+		sleepInSecond(5);
+		List<WebElement> lisSelecttNumber = driver.findElements(By.xpath("//ol[@id='selectable']/li[@class='ui-state-default ui-selectee ui-selected']"));
+		Assert.assertEquals(lisSelecttNumber.size(), 4);
+	}
+	
+	
+	
+	
 	
 	
 	
